7197fd1602668cf73037a0c94409244e01402197
Carlos Andrade <andradecarlos2503@hotmail.com>
2025-12-11 23:01:20 -0500

    Update alert message with a more encouraging text

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ onload = () => {
   document.body.classList.remove("container");
 
   setTimeout(() => {
-    alert("Estas bien preciosa.💗");
+    alert("Puede que tu semana vaya mal, pero tú eres muy capaz, y sobre todo eres muy preciosa, yo se que puedes con todo💗");
     window.location.href = "https://music.youtube.com/watch?v=gq-FRSNgK4I&si=VXy1O2u0EvbicJZZ"; // Reemplaza con el enlace de tu elección
   }, 10000); // 10000 milisegundos
 };
